81f635867b7df1c490e43947d329fdfe2fb3ff83
tony <111899329+Razertrooper@users.noreply.github.com>
2023-08-30 08:11:22 -0700

    AL Update Website

--- Professional Website ANTLEON/scroll.js
+++ Professional Website ANTLEON/scroll.js
@@ -1,28 +1,36 @@
 // JavaScript source code
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function () { scrollFunction() };
+const windowInnerWidth = window.innerWidth;
+
+if (windowInnerWidth > 800) {
+    window.onscroll = function () { scrollFunction() };
+}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        document.getElementById("headerh1").style.fontSize = "18px";
-        document.getElementById("headerh1").style.padding = "0px";
-        document.getElementById("headerh1").style.lineHeight = "32px";
-        document.getElementById("navlink").style.margin = "0px";
-        document.getElementById("navlink").style.fontSize = "14px";
-        document.getElementsByClassName("listlink")[0].style.padding = "5px";
-        document.getElementsByClassName("listlink")[1].style.padding = "5px";
-        document.getElementsByClassName("listlink")[2].style.padding = "5px";
-        document.getElementsByClassName("listlink")[3].style.padding = "5px";
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
 
-    } else {
-        document.getElementById("headerh1").style.fontSize = "32px";
-        document.getElementById("headerh1").style.padding = "10px";
-        document.getElementById("headerh1").style.lineHeight = "36px";
-        document.getElementById("navlink").style.margin = "16px 0px 16px 55%";
-        document.getElementById("navlink").style.fontSize = "16px";
-        document.getElementsByClassName("listlink")[0].style.padding = "15px";
-        document.getElementsByClassName("listlink")[1].style.padding = "15px";
-        document.getElementsByClassName("listlink")[2].style.padding = "15px";
-        document.getElementsByClassName("listlink")[3].style.padding = "15px";
-    }
+            document.getElementById("headert").style.width = "100%";
+
+
+            document.getElementById("headerh1").innerHTML = "AL";
+            document.getElementById("headerh1").style.margin = "0px";
+
+            document.getElementById("navlink").style.margin = "0px";
+
+
+ 
+
+        } else {
+
+            document.getElementById("headert").style.width = "100%";
+
+            document.getElementById("headerh1").innerHTML = "Anthony's Website";
+            document.getElementById("headerh1").style.margin = "15px";
+
+            document.getElementById("navlink").style.margin = "15px";
+            document.getElementById("navlink").style.marginLeft = "55%";
+
+
+            
+        }
 }
